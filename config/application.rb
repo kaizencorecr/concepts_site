f021af714ebfd5b9fb7b56bf5b6c5e3baaa316ae
Pablo Bonilla <pabonillavargas@gmail.com>
2016-03-25 00:36:09 -0600

    Changed the navbar and the carousel behaviour to meet the new design

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module ConceptsSite
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.precompile += %w(.png)
+    config.assets.precompile += %w(.png, .svg, .jpg)
   end
 end
